364d3e6b56259d2dcad16bf02b33c45e11f7bc71
Jihane Monsif <jihane.monsif@gmail.com>
2026-07-12 21:49:36 +0100

    Ajout du dashboard dynamique

--- irrigation/views.py
+++ irrigation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Parcelle, Culture, Meteo, Irrigation
 
-# Create your views here.
+def home(request):
+    context = {
+        'parcelles': Parcelle.objects.count(),
+        'cultures': Culture.objects.count(),
+        'meteo': Meteo.objects.count(),
+        'irrigations': Irrigation.objects.count(),
+    }
+
+    return render(request, 'irrigation/home.html', context)
